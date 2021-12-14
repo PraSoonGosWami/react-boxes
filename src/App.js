a9071b75e7f4b2d327f4aa86d4a959714fc61b6e
Prasoon <Prasoon@Prasoons-MacBook-Pro.local>
2021-12-14 14:02:48 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [drag, setDrag] = useState(false);
+
+  const mup = (e) => {
+    setDrag(false);
+    if (e.target.className === "box") e.target.style.backgroundColor = "black";
+  };
+
+  const mdown = (e) => {
+    setDrag(true);
+    if (e.target.className === "box") e.target.style.backgroundColor = "black";
+  };
+  const mover = (e) => {
+    if (drag && e.target.className === "box")
+      e.target.style.backgroundColor = "black";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      onMouseUp={mup}
+      onMouseDown={mdown}
+      onMouseOver={mover}
+    >
+      {Array(324)
+        .fill(1)
+        .map((_, index) => {
+          return (
+            <div
+              className="box"
+              key={index}
+              style={{
+                width: 20,
+                height: 20,
+                margin: 2,
+                backgroundColor: "#ddd",
+              }}
+            ></div>
+          );
+        })}
     </div>
   );
 }
